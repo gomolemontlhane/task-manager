ab04074e6195ca249f8676f242a0ef706f283693
Gomolemo Ntlhane <gomolemontlhane@gmail.com>
2025-01-18 01:02:52 +0200

    created sub-folder of app called pages using ng generate component pages/TaskView command

---
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-view',
+  imports: [],
+  templateUrl: './task-view.component.html',
+  styleUrl: './task-view.component.scss'
+})
+export class TaskViewComponent {
+
+}
